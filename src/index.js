e06cb343520afa4e54307468546bf5be5c13da35
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-08-17 16:36:37 +0300

    додае змінні

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
 
+var debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
+
+const url = 'https://restcountries.com/v3.1/name/';
+
+const searchParams = [name.official, capital, population, flags.svg, languages];
+
+const refs = {
+  inputName: document.querySelector('#search-box'),
+};
+
+refs.inputName.addEventListener(
+  'input',
+  debounce(fetchCountries, DEBOUNCE_DELAY)
+);
+
+function fetchCountries(name) {}
